d45e1a46b72c49e0fa827290370b38efad3e4752
Jonathan Warburton <jonathan.warburton09@gmail.com>
2019-12-03 17:32:37 +0100

    Add day 3 puzzle 2 solution

--- src/main/scala/com/jswarburton/adventofcode/day03/CrossedWires.scala
+++ src/main/scala/com/jswarburton/adventofcode/day03/CrossedWires.scala
@@ -16,10 +16,29 @@ case class Point(x: Int, y: Int) {
 }
 
 object CrossedWires {
-
   def puzzle1(filePath: String): Int = {
     val (wire1Path, wire2Path) = read(filePath)
     manhattanDistanceToNearestIntersection(wire1Path, wire2Path)
+  }
+
+  def puzzle2(filePath: String): Int = {
+    val (wire1Path, wire2Path) = read(filePath)
+    minWireLengthToIntersection(wire1Path, wire2Path)
+  }
+
+  def minWireLengthToIntersection(wire1Path: List[DirectionAndDistance],
+                                  wire2Path: List[DirectionAndDistance]): Int = {
+    val seenPointsAndWireLength1 = genSeenPointsAndWireLength(wire1Path)
+    val seenPointsAndWireLength2 = genSeenPointsAndWireLength(wire2Path)
+
+    val intersectionPoints = seenPointsAndWireLength1.keySet.intersect(seenPointsAndWireLength2.keySet)
+
+    intersectionPoints.foldLeft(Integer.MAX_VALUE) {
+      (minSoFar, point) => {
+        val totalWireLength = seenPointsAndWireLength1(point) + seenPointsAndWireLength2(point)
+        if (totalWireLength < minSoFar) totalWireLength else minSoFar
+      }
+    }
   }
 
   def read(filePath: String): (List[DirectionAndDistance], List[DirectionAndDistance]) = {
@@ -34,42 +53,56 @@ object CrossedWires {
 
   def manhattanDistanceToNearestIntersection(wire1Path: List[DirectionAndDistance],
                                              wire2Path: List[DirectionAndDistance]): Int = {
-    @tailrec
-    def genSeenPoints(remainingDirections: List[DirectionAndDistance],
-                      currentPoint: Point = Point(0, 0),
-                      seenPoints: Set[Point] = Set()): Set[Point] = {
-
-      remainingDirections match {
-        case Nil => seenPoints
-        case dirs => {
-          val nextDir = dirs.head
-
-          val distance = nextDir.distance
-
-          val (newSeenPoints, newCurrentPoint) =
-            nextDir.direction match {
-              case 'U' => ((1 to distance).toSet.map(currentPoint.up), currentPoint.up(distance))
-
-              case 'D' => ((1 to distance).toSet.map(currentPoint.down), currentPoint.down(distance))
-
-              case 'L' => ((1 to distance).toSet.map(currentPoint.left), currentPoint.left(distance))
-
-              case 'R' => ((1 to distance).toSet.map(currentPoint.right), currentPoint.right(distance))
-            }
-
-          genSeenPoints(remainingDirections.tail, newCurrentPoint, seenPoints ++ newSeenPoints)
-        }
-      }
-    }
-
-    val seenPoints1 = genSeenPoints(wire1Path)
-    val seenPoints2 = genSeenPoints(wire2Path)
+    val seenPoints1 = genSeenPointsAndWireLength(wire1Path).keySet
+    val seenPoints2 = genSeenPointsAndWireLength(wire2Path).keySet
 
     val intersections = seenPoints1.intersect(seenPoints2)
 
     val manhattanDistances = intersections.toList.map { case Point(x, y) => Math.abs(x) + Math.abs(y) }
 
     manhattanDistances.min
+  }
+
+  @tailrec
+  private def genSeenPointsAndWireLength(remainingDirections: List[DirectionAndDistance],
+                                         currentPoint: Point = Point(0, 0),
+                                         currentLength: Int = 0,
+                                         seenPointsAndLengths: Map[Point, Int] = Map()): Map[Point, Int] = {
+
+    remainingDirections match {
+      case Nil => seenPointsAndLengths
+      case dirs => {
+        val nextDir = dirs.head
+
+        val distance = nextDir.distance
+
+        val (newSeenPointsAndLengths, newCurrentPoint) =
+          nextDir.direction match {
+            case 'U' => ((1 to distance).map(a => currentPoint.up(a) -> (currentLength + a)).toMap,
+              currentPoint.up(distance))
+
+            case 'D' => ((1 to distance).map(a => currentPoint.down(a) -> (currentLength + a)).toMap,
+              currentPoint.down(distance))
+
+            case 'L' => ((1 to distance).map(a => currentPoint.left(a) -> (currentLength + a)).toMap,
+              currentPoint.left(distance))
+
+            case 'R' => ((1 to distance).map(a => currentPoint.right(a) -> (currentLength + a)).toMap,
+              currentPoint.right(distance))
+          }
+
+        val allSeenAndLengths = newSeenPointsAndLengths.foldLeft(seenPointsAndLengths) {
+          case (acc, (point, distance)) =>
+            if (acc.contains(point)) acc
+            else acc + ((point, distance))
+        }
+
+        genSeenPointsAndWireLength(remainingDirections.tail,
+          newCurrentPoint,
+          currentLength + distance,
+          allSeenAndLengths)
+      }
+    }
   }
 
 }
