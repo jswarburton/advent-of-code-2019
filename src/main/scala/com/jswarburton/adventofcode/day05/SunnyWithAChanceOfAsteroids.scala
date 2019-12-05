09042e12530a832a1401e298950ea0898c4f6453
Jonathan Warburton <jonathan.warburton09@gmail.com>
2019-12-05 21:43:18 +0100

    More day 5 tidy up

--- src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
+++ src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
@@ -38,21 +38,21 @@ object SunnyWithAChanceOfAsteroids {
         // Add
         case 1 =>
           val newValue = read(mode1, 1) + read(mode2, 2)
-          val newMemory = write(3, newValue)
-          helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
+          val newLatest = write(3, newValue)
+          helper(latest = newLatest, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
 
         // Multiply
         case 2 =>
           val newValue = read(mode1, 1) * read(mode2, 2)
-          val newMemory = write(3, newValue)
-          helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
+          val newLatest = write(3, newValue)
+          helper(latest = newLatest, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
 
         // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
         // For example, the instruction 3,50 would take an input value and store it at address 50.
         case 3 =>
           val input :: newInputs = inputInstructions
-          val newMemory = write(1, input)
-          helper(latest = newMemory, pointer = pointer + 2, inputInstructions = newInputs, outputs = outputs)
+          val newLatest = write(1, input)
+          helper(latest = newLatest, pointer = pointer + 2, inputInstructions = newInputs, outputs = outputs)
 
         // Opcode 4 outputs the value of its only parameter.
         // For example, the instruction 4,50 would output the value at address 50.
@@ -74,14 +74,14 @@ object SunnyWithAChanceOfAsteroids {
         // Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
         case 7 =>
           val newValue = if (read(mode1, 1) < read(mode2, 2)) 1 else 0
-          val newMemory = write(3, newValue)
-          helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
+          val newLatest = write(3, newValue)
+          helper(latest = newLatest, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
 
         // Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
         case 8 =>
           val newValue = if (read(mode1, 1) == read(mode2, 2)) 1 else 0
-          val newMemory = write(3, newValue)
-          helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
+          val newLatest = write(3, newValue)
+          helper(latest = newLatest, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
 
         case 99 => outputs
       }
@@ -93,11 +93,10 @@ object SunnyWithAChanceOfAsteroids {
   def parseOpCodeAndModes(value: Int): (Int, ParameterMode, ParameterMode, ParameterMode) = {
     val opCode = value % 100
 
-    def mapToMode(i: Int): ParameterMode =
-      i match {
-        case 0 => PositionMode
-        case 1 => ImmediateMode
-      }
+    def mapToMode(i: Int): ParameterMode = i match {
+      case 0 => PositionMode
+      case 1 => ImmediateMode
+    }
 
     val mode1 = value / 100 % 10
     val mode2 = value / 1000 % 10
@@ -107,5 +106,4 @@ object SunnyWithAChanceOfAsteroids {
   }
 
   def puzzle2(filePath: String): List[Int] = run(read(filePath), inputInstruction = 5)
-
 }
