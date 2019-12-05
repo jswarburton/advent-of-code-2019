2ed65d5bbfe32e40fce48a53ed2239e7cafbdb8d
Jonathan Warburton <jonathan.warburton09@gmail.com>
2019-12-05 15:11:34 +0100

    Day 5 Part 2 Solution (with a bit of help...)

--- src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
+++ src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
@@ -19,75 +19,79 @@ object SunnyWithAChanceOfAsteroids {
       .toList
 
   def run(data: List[Int], inputInstruction: Int): List[Int] = {
-    def calculateNewIndexAndValueOps1And2(seq: IndexedSeq[Int],
-                                          i: Int,
-                                          op: (Int, Int) => Int,
-                                          modes: (ParameterMode, ParameterMode)): (Int, Int) = {
-      val firstValue = modes._1 match {
-        case PositionMode => seq(seq(i + 1))
-
-        case ImmediateMode => seq(i + 1)
-      }
-
-      val secondValue = modes._2 match {
-        case PositionMode => seq(seq(i + 2))
-
-        case ImmediateMode => seq(i + 2)
-      }
-
-      val destinationIndex = seq(i + 3)
-
-      (destinationIndex, op(firstValue, secondValue))
-    }
-
     @tailrec
     def helper(latest: IndexedSeq[Int],
-               currIndex: Int,
-               inputInstruction: Int,
-               outputs: List[Int]): List[Int] = {
-      if (currIndex > latest.size) outputs
-      else {
-        val (opCode, mode1, mode2, _) = parseOpCodeAndModes(latest(currIndex))
+               inputInstructions: List[Int],
+               outputs: List[Int] = List(),
+               pointer: Int = 0): List[Int] = {
 
-        opCode match {
-          case 1 =>
-            val (destinationIndex, destinationValue) = calculateNewIndexAndValueOps1And2(latest, currIndex, _ + _, (mode1, mode2))
-            helper(latest.updated(destinationIndex, destinationValue),
-              currIndex + 4,
-              inputInstruction = latest(currIndex + 4),
-              outputs)
+      val (opCode, mode1, mode2, _) = parseOpCodeAndModes(latest(pointer))
 
-          case 2 =>
-            val (destinationIndex, destinationValue) = calculateNewIndexAndValueOps1And2(latest, currIndex, _ * _, (mode1, mode2))
-            helper(
-              latest.updated(destinationIndex, destinationValue),
-              currIndex + 4,
-              inputInstruction = latest(currIndex + 4),
-              outputs)
+      def instruction(i: Int): Int = latest(pointer + i)
 
-          /*
-          Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
-          For example, the instruction 3,50 would take an input value and store it at address 50.
-           */
-          case 3 => helper(latest.updated(latest(currIndex + 1), inputInstruction), currIndex + 2, inputInstruction = latest(currIndex + 2), outputs)
+      def param(i: Int): Int = instruction(i + 1)
 
-          /*
-          Opcode 4 outputs the value of its only parameter.
-          For example, the instruction 4,50 would output the value at address 50.
-           */
-          case 4 =>
-            val newOutput = mode1 match {
-              case PositionMode => latest(latest(currIndex + 1))
-              case ImmediateMode => latest(currIndex + 1)
-            }
-            helper(latest, currIndex + 2, inputInstruction = latest(currIndex + 2), newOutput :: outputs)
+      def read(mode: ParameterMode, i: Int): Int = mode match {
+        case PositionMode => latest(param(i))
+        case ImmediateMode => param(i)
+      }
 
-          case 99 => outputs
-        }
+      def write(i: Int, value: Int): IndexedSeq[Int] = latest.updated(param(i), value)
+
+      opCode match {
+        // Add
+        case 1 =>
+          val newValue = read(mode1, 0) + read(mode2, 1)
+          val newMemory = write(2, newValue)
+          helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
+
+        // Multiply
+        case 2 =>
+          val newValue = read(mode1, 0) * read(mode2, 1)
+          val newMemory = write(2, newValue)
+          helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
+
+        // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
+        // For example, the instruction 3,50 would take an input value and store it at address 50.
+        case 3 => // input
+          val input :: newInputs = inputInstructions
+          val newMemory = write(0, input)
+          helper(latest = newMemory, pointer = pointer + 2, inputInstructions = newInputs, outputs = outputs)
+
+        // Opcode 4 outputs the value of its only parameter.
+        // For example, the instruction 4,50 would output the value at address 50.
+        case 4 =>
+          val newValue = read(mode1, 0)
+          val newOutputs = newValue :: outputs
+          helper(pointer = pointer + 2, outputs = newOutputs, inputInstructions = inputInstructions, latest = latest)
+
+        // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+        case 5 =>
+          if (read(mode1, 0) != 0) helper(pointer = read(mode2, 1), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+          else helper(pointer = pointer + 3, outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+
+        // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+        case 6 =>
+          if (read(mode1, 0) == 0) helper(pointer = read(mode2, 1), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+          else helper(pointer = pointer + 3, outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+
+        // Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+        case 7 =>
+          val newValue = if (read(mode1, 0) < read(mode2, 1)) 1 else 0
+          val newMemory = write(2, newValue)
+          helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
+
+        // Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+        case 8 =>
+          val newValue = if (read(mode1, 0) == read(mode2, 1)) 1 else 0
+          val newMemory = write(2, newValue)
+          helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
+
+        case 99 => outputs
       }
     }
 
-    helper(data.toIndexedSeq, currIndex = 0, inputInstruction, List())
+    helper(data.toIndexedSeq, List(inputInstruction))
   }
 
   def parseOpCodeAndModes(value: Int): (Int, ParameterMode, ParameterMode, ParameterMode) = {
@@ -105,5 +109,7 @@ object SunnyWithAChanceOfAsteroids {
 
     (opCode, mapToMode(mode1), mapToMode(mode2), mapToMode(mode3))
   }
+
+  def puzzle2(filePath: String): List[Int] = run(read(filePath), inputInstruction = 5)
 
 }
--- src/test/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroidsTest.scala
+++ src/test/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroidsTest.scala
@@ -1,6 +1,6 @@
 package com.jswarburton.adventofcode.day05
 
-import com.jswarburton.adventofcode.day05.SunnyWithAChanceOfAsteroids.{parseOpCodeAndModes, puzzle1}
+import com.jswarburton.adventofcode.day05.SunnyWithAChanceOfAsteroids.{parseOpCodeAndModes, puzzle1, puzzle2}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -21,5 +21,57 @@ class SunnyWithAChanceOfAsteroidsTest extends AnyFlatSpec with Matchers {
     head shouldBe 13787043
     tail.forall(_ == 0) shouldBe true
   }
+  it should "produce correct result for puzzle 2" in {
+    val head :: tail = puzzle2(filePath)
 
+    head shouldBe 3892695
+    tail.forall(_ == 0) shouldBe true
+  }
+
+  it should "produce correct results for examples given" in {
+    val positionModeEqualTo8 = List(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8)
+    evaluate(positionModeEqualTo8, inputInstruction = 8, expectedHead = 1)
+    evaluate(positionModeEqualTo8, inputInstruction = 6, expectedHead = 0)
+
+    val positionModeLessThan8 = List(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8)
+    evaluate(positionModeLessThan8, inputInstruction = 8, expectedHead = 0)
+    evaluate(positionModeLessThan8, inputInstruction = 9, expectedHead = 0)
+    evaluate(positionModeLessThan8, inputInstruction = 7, expectedHead = 1)
+
+    val immediateModeEqualTo8 = List(3, 3, 1108, -1, 8, 3, 4, 3, 99)
+    evaluate(immediateModeEqualTo8, inputInstruction = 8, expectedHead = 1)
+    evaluate(immediateModeEqualTo8, inputInstruction = 6, expectedHead = 0)
+
+    val immediateModeLessThan8 = List(3, 3, 1107, -1, 8, 3, 4, 3, 99)
+    evaluate(immediateModeLessThan8, inputInstruction = 8, expectedHead = 0)
+    evaluate(immediateModeLessThan8, inputInstruction = 9, expectedHead = 0)
+    evaluate(immediateModeLessThan8, inputInstruction = 7, expectedHead = 1)
+  }
+
+  it should "produce correct result for larger example" in {
+    val input = List(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+      1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+      999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99)
+
+    evaluate(input, inputInstruction = 7, expectedHead = 999)
+    evaluate(input, inputInstruction = 8, expectedHead = 1000)
+    evaluate(input, inputInstruction = 9, expectedHead = 1001)
+  }
+
+  it should "output 0 if input is zero, 1 otherwise" in {
+    val inputUsingPositionMode = List(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9)
+    evaluate(inputUsingPositionMode, inputInstruction = 0, expectedHead = 0)
+    evaluate(inputUsingPositionMode, inputInstruction = 5, expectedHead = 1)
+
+    val inputUsingImmediateMode = List(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1)
+    evaluate(inputUsingImmediateMode, inputInstruction = 0, expectedHead = 0)
+    evaluate(inputUsingImmediateMode, inputInstruction = 5, expectedHead = 1)
+  }
+
+  private def evaluate(input: List[Int], inputInstruction: Int, expectedHead: Int): Unit = {
+    val res = SunnyWithAChanceOfAsteroids.run(data = input, inputInstruction = inputInstruction)
+
+    res.head shouldBe expectedHead
+    res.tail.forall(_ == 0) shouldBe true
+  }
 }
