83f518c904cae727c870e0492112b8701adcc6af
Jonathan Warburton <jonathan.warburton09@gmail.com>
2019-12-05 21:40:47 +0100

    Make off-by-one errors less confusing

--- src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
+++ src/main/scala/com/jswarburton/adventofcode/day05/SunnyWithAChanceOfAsteroids.scala
@@ -28,59 +28,59 @@ object SunnyWithAChanceOfAsteroids {
       val (opCode, mode1, mode2, _) = parseOpCodeAndModes(latest(pointer))
 
       def read(mode: ParameterMode, i: Int): Int = mode match {
-        case PositionMode => latest(latest(pointer + i + 1))
-        case ImmediateMode => latest(pointer + i + 1)
+        case PositionMode => latest(latest(pointer + i))
+        case ImmediateMode => latest(pointer + i)
       }
 
-      def write(i: Int, value: Int): Vector[Int] = latest.updated(latest(pointer + i + 1), value)
+      def write(i: Int, value: Int): Vector[Int] = latest.updated(latest(pointer + i), value)
 
       opCode match {
         // Add
         case 1 =>
-          val newValue = read(mode1, 0) + read(mode2, 1)
-          val newMemory = write(2, newValue)
+          val newValue = read(mode1, 1) + read(mode2, 2)
+          val newMemory = write(3, newValue)
           helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
 
         // Multiply
         case 2 =>
-          val newValue = read(mode1, 0) * read(mode2, 1)
-          val newMemory = write(2, newValue)
+          val newValue = read(mode1, 1) * read(mode2, 2)
+          val newMemory = write(3, newValue)
           helper(latest = newMemory, inputInstructions = inputInstructions, outputs = outputs, pointer = pointer + 4)
 
         // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
         // For example, the instruction 3,50 would take an input value and store it at address 50.
         case 3 =>
           val input :: newInputs = inputInstructions
-          val newMemory = write(0, input)
+          val newMemory = write(1, input)
           helper(latest = newMemory, pointer = pointer + 2, inputInstructions = newInputs, outputs = outputs)
 
         // Opcode 4 outputs the value of its only parameter.
         // For example, the instruction 4,50 would output the value at address 50.
         case 4 =>
-          val newValue = read(mode1, 0)
+          val newValue = read(mode1, 1)
           val newOutputs = newValue :: outputs
           helper(pointer = pointer + 2, outputs = newOutputs, inputInstructions = inputInstructions, latest = latest)
 
         // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
         case 5 =>
-          if (read(mode1, 0) != 0) helper(pointer = read(mode2, 1), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+          if (read(mode1, 1) != 0) helper(pointer = read(mode2, 2), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
           else helper(pointer = pointer + 3, outputs = outputs, inputInstructions = inputInstructions, latest = latest)
 
         // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
         case 6 =>
-          if (read(mode1, 0) == 0) helper(pointer = read(mode2, 1), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
+          if (read(mode1, 1) == 0) helper(pointer = read(mode2, 2), outputs = outputs, inputInstructions = inputInstructions, latest = latest)
           else helper(pointer = pointer + 3, outputs = outputs, inputInstructions = inputInstructions, latest = latest)
 
         // Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
         case 7 =>
-          val newValue = if (read(mode1, 0) < read(mode2, 1)) 1 else 0
-          val newMemory = write(2, newValue)
+          val newValue = if (read(mode1, 1) < read(mode2, 2)) 1 else 0
+          val newMemory = write(3, newValue)
           helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
 
         // Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
         case 8 =>
-          val newValue = if (read(mode1, 0) == read(mode2, 1)) 1 else 0
-          val newMemory = write(2, newValue)
+          val newValue = if (read(mode1, 1) == read(mode2, 2)) 1 else 0
+          val newMemory = write(3, newValue)
           helper(latest = newMemory, pointer = pointer + 4, outputs = outputs, inputInstructions = inputInstructions)
 
         case 99 => outputs
